8c14af171324a0bcb731b17f7cf81cc64dd6bb27
Hugh Lilly <hughlilly@users.noreply.github.com>
2021-06-25 13:18:25 +1200

    added logging

--- getORCIDToken.py
+++ getORCIDToken.py
@@ -1,11 +1,32 @@
 import requests
-import time
 from decouple import config
+import logging
 
-ORCID_API_ID = config('ORCID_API_ID')
-ORCID_API_SECRET = config('ORCID_API_SECRET')
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+
+logfile_format = logging.Formatter('%(asctime)s: %(name)s (%(funcName)s): %(message)s')
+logfile = logging.FileHandler(  filename='getORCIDtoken.log',
+                                mode='a',
+                                encoding=None,
+                                delay=False )
+logfile.setFormatter(logfile_format)
+logfile.setLevel(logging.DEBUG)
+
+log.addHandler(stream_handler)
+log.addHandler(logfile)
 
 testing = False
+
+if testing:
+    ORCID_API_ID = config('ORCID_SANDBOX_API_ID')
+    ORCID_API_SECRET = config('ORCID_SANDBOX_API_SECRET')
+else:
+    ORCID_API_ID = config('ORCID_API_ID')
+    ORCID_API_SECRET = config('ORCID_API_SECRET')
 
 def fetchToken(id, secret, sandbox):
     auth = {
@@ -13,37 +34,44 @@ def fetchToken(id, secret, sandbox):
         'client_secret': secret,
         'grant_type': 'client_credentials',
         'scope': '/read-public'
-        }
+    }
     headers = {
         'accept': 'application/json'
     }
-    
+
     if sandbox == True:
-        service = "sandbox."
-        print ("NB: Running in testing mode (using sandbox).")
+        service = 'sandbox.'
+        log.info('In testing mode (using sandbox)')
     else:
         service = ''
+        log.info('In production mode')
 
     tokenURL = 'https://' + service + 'orcid.org/oauth/token'
 
-    response = requests.post(
-        url=tokenURL,
-        headers=headers,
-        data=auth
-    )
+    try:
+        response = requests.post(   url=tokenURL,
+                                    headers=headers,
+                                    data=auth )
+        response.raise_for_status()
+        if response.status_code == 200:
+            ORCID_ACCESS_TOKEN = response.json()['access_token']
+            log.info('Retrieving token from {0}...'.format(tokenURL))
+            log.debug('Response code {0}'.format(response.status_code))
+            return ORCID_ACCESS_TOKEN
+    except requests.RequestException as error:
+        log.exception(error)
+        raise
 
-    print('Retrieving token from {0}...'.format(tokenURL))
+def writeToken(token, testing):
+    with open(".env", "a") as file:
+        if testing == True:
+            file.write("\n# Sandbox token\n")
+        token_env = ('PUBLIC_ACCESS_TOKEN = \"{0}\"'.format(token))
+        file.write(token_env)
+        log.info("Added token to .env file.")
+        file.close
 
-    if response.status_code == 200:
-        ORCID_ACCESS_TOKEN = response.json()['access_token']
-        return ORCID_ACCESS_TOKEN
-    else:
-        raise Exception('Error: {0} {1}'.format(response.status_code, response.text))
-
-token = fetchToken(ORCID_API_ID, ORCID_API_SECRET, testing)
-
-with open(".env", "a") as file:
-    newline = ("PUBLIC_ACCESS_TOKEN = \"{0}\"".format(token))
-    file.write(newline)
-    print ("Added token '{0}' to .env file.".format(token))
-    file.close
+if __name__ == '__main__':
+    token = fetchToken(ORCID_API_ID, ORCID_API_SECRET, testing)
+    log.info("Fetched token.")
+    writeToken(token, testing)
